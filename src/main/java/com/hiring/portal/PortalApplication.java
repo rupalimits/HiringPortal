82c1c9f2a363421b79782e0679fcd53d9fb28392
rupalimits <31814652+rupalimits@users.noreply.github.com>
2021-11-13 19:09:48 +0400

    Add files via upload
    Initial Commit

---
+++ src/main/java/com/hiring/portal/PortalApplication.java
@@ -0,0 +1,13 @@
+package com.hiring.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalApplication {
+	// Welcome to the new Hiring portal 
+	public static void main(String[] args) {
+		SpringApplication.run(PortalApplication.class, args);
+	}
+
+}
